75432d38acaf8febe1a3ec4a58b52ef7be5cc8c7
Gotty <78111224+iGotty@users.noreply.github.com>
2023-10-25 23:58:28 -0500

    Mejoras basicas

--- src/components/EntrenamientoModelo.js
+++ src/components/EntrenamientoModelo.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './EntrenamientoModelo.css';
 import confusion from './assets/confus.png';
 import miFondo from './assets/miFondo.png';
 
-
 function EntrenamientoModelo() {
+    // Inicializar el estado con valores por defecto
+    const [metrics, setMetrics] = useState({
+        f1_score: 0,
+        precision: 0,
+        recall: 0
+    });
+
+    // Usar useEffect para obtener los datos de la API cuando el componente se monte
+    useEffect(() => {
+        // Definir una función asincrónica dentro de useEffect
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://127.0.0.1:8000/metrics");
+                const data = await response.json();
+                setMetrics(data); // Actualizar el estado con los datos obtenidos
+            } catch (error) {
+                console.error("Error al obtener los datos de la API:", error);
+            }
+        };
+
+        fetchData(); // Ejecutar la función
+    }, []); // El arreglo vacío [] significa que useEffect se ejecutará solo una vez, cuando el componente se monte
+
     return (
-        <div className='contenedor-super-externo' >
+        <div className='contenedor-super-externo'>
             <div className='contenedor-externo'>
                 <div className="columna-izquierda">
                     <img src={confusion} alt="confusion" className="imagen-confusion" /> 
                     <div className="display">
-                    <span>F1 Score:</span> <span className="display-value">0.95</span>
+                        <span>F1 Score:</span> <span className="display-value">{metrics.f1_score}</span>
                     </div>
                     <div className="display">
-                    <span>Precision:</span> <span className="display-value">0.93</span>
+                        <span>Precision:</span> <span className="display-value">{metrics.precision}</span>
                     </div>
                     <div className="display">
-                    <span>Recall:</span> <span className="display-value">0.97</span>
+                        <span>Recall:</span> <span className="display-value">{metrics.recall}</span>
                     </div>
                 </div>
                 <div className="columna-derecha">
--- src/components/MiComponente.js
+++ src/components/MiComponente.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './MiComponente.css';
 
 // Suponiendo que las imágenes están en la carpeta 'assets'
@@ -7,13 +8,119 @@ import ods7 from './assets/ods7.png';
 import ods16 from './assets/ods16.png';
 
 function MiComponente() {
-    const [texto, setTexto] = useState("");
-    const [prediccion, setPrediccion] = useState("El texto ingresado pertenece al ODS 6.");
+    
 
-    const enviarTexto = () => {
-        // Aquí puedes agregar la lógica para enviar el texto al modelo de machine learning
-        // y establecer el resultado en setPrediccion.
+    const [texto, setTexto] = useState("");
+    const [prediccion, setPrediccion] = useState("");
+    const subirCsv = () => {
+        // Simula un clic en el input para seleccionar un archivo
+        document.getElementById('fileInput').click();
     }
+    const [nombreArchivo, setNombreArchivo] = useState("");
+    const [uploadPercentage, setUploadPercentage] = useState(0);
+
+    const readFile = (file) => {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            // Parsea el contenido del CSV
+            const lines = event.target.result.split('\n').map(line => line.trim()).filter(line => line);
+            sendToAPI(lines);
+        };
+        reader.readAsText(file);
+    };
+    
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setNombreArchivo(file.name);
+            readFile(file);
+        }
+    };
+    
+    const sendToAPI = (texts) => {
+        setIsProcessing(true);  // Inicia el procesamiento
+    
+        axios.post('http://127.0.0.1:8000/predict', {
+            texts: texts
+        }, {
+            onUploadProgress: (progressEvent) => {
+                const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                setUploadPercentage(percentCompleted);
+            }
+        })
+        .then(response => {
+            downloadPredictions(response.data["predictions" ], texts);
+        })
+        .catch(error => {
+            console.error("Error al enviar los datos a la API:", error);
+        })
+        .finally(() => {
+            setIsProcessing(false);  // Finaliza el procesamiento
+            setUploadPercentage(0);  // Resetea el porcentaje de subida
+            setNombreArchivo("");    // Resetea el nombre del archivo
+            document.getElementById('fileInput').value = null;  // Limpia el input file
+        });
+    };
+    
+    
+    
+    const enviarTexto = async () => {
+        // Definir la URL de la API
+        const apiUrl = "http://127.0.0.1:8000/predict";
+
+        try {
+            const response = await fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "Origin": "http://localhost:3000",
+                },
+                body: JSON.stringify({ texts: [texto] })
+            });
+
+            const data = await response.json();
+            if (data.predictions[0]) {
+                setPrediccion(`El texto ingresado pertenece al ODS ${data.predictions[0]}.`);
+            } else {
+                console.error("No se pudo obtener la predicción de la API");
+            }
+        } catch (error) {
+            console.error("Hubo un error al enviar el texto a la API:", error);
+        }
+    }
+
+    const [isProcessing, setIsProcessing] = useState(false);
+
+
+    const downloadPredictions = (data, texts) => {
+        if(data.length !== texts.length) {
+            console.error("Los tamaños de las listas 'data' y 'texts' no coinciden.");
+            return;
+        }
+    
+        // Crear contenido CSV
+        let csvContent = "data,texts\n"; // Esta es la cabecera del archivo
+        for(let i = 0; i < data.length; i++) {
+            // Asegurarse de que los datos no tengan comas ni comillas dobles
+            // (esto es para simplificar y evitar problemas en un CSV básico)
+            const cleanData = String(data[i]).replace(/,|"/g, ' ');
+            const cleanText = String(texts[i]).replace(/,|"/g, ' ');
+    
+            csvContent += `"${cleanText}",${cleanData}\n`; // Agregar cada par data-text al contenido CSV
+        }
+    
+        const blob = new Blob([csvContent], {type: 'text/csv'});
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'predictions.csv');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+    
 
     return (
         <div className='contenedor-super-externo'>
@@ -22,9 +129,9 @@ function MiComponente() {
                     <h1>Introduce un texto aquí ...</h1>
                     <p>El texto ingresado será transformado y luego procesado a través de un modelo de machine learning para predecir a qué ODS pertenece.</p>
                     <div className="imagenes">
-                        <img src={ods6} alt="ODS 6" className="imagen-ods6" />
-                        <img src={ods7} alt="ODS 7" className="imagen-ods7" />
-                        <img src={ods16} alt="ODS 16" className="imagen-ods16" />
+                        <img src={ods6} alt="ODS 6" className={`imagen-ods6 ${prediccion.includes("ODS 6") ? 'activo' : ''}`} />
+                        <img src={ods7} alt="ODS 7" className={`imagen-ods7 ${prediccion.includes("ODS 7") ? 'activo' : ''}`} />
+                        <img src={ods16} alt="ODS 16" className={`imagen-ods16 ${prediccion.includes("ODS 16") ? 'activo' : ''}`} />
                     </div>
                     <textarea 
                         placeholder="Ingresar texto"
@@ -35,14 +142,35 @@ function MiComponente() {
                         {prediccion}
                     </div>
                     <hr />
+                    
+                    {/* Aquí agregamos el input oculto para subir archivos */}
+                    <input 
+                        type="file" 
+                        accept=".csv" 
+                        id="fileInput" 
+                        style={{display: 'none'}} 
+                        onChange={handleFileChange}
+                    />
+    
                     <div className="botones">
-                        <button className="btn btn-gris">Subir .csv</button>
+                        <button className="btn btn-gris" onClick={subirCsv}>
+                            {isProcessing ? (
+                                <div className='holahola'>
+                                    <p className='proccc'> Procesando </p><div className="loader"></div>
+                                </div>
+                            ) : "Subir .csv"}
+                        </button>
+
                         <button className="btn btn-verde" onClick={enviarTexto}>Enviar</button>
                     </div>
+                   
+
+
                 </div>
             </div>
         </div>
     );
+    
 }
 
 export default MiComponente;
