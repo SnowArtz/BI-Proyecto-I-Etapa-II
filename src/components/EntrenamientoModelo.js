f658c7125c5b2ab54755c7983a8d184550121170
Gotty <78111224+iGotty@users.noreply.github.com>
2023-10-29 18:13:56 -0500

    Cambios finales

--- src/components/EntrenamientoModelo.js
+++ src/components/EntrenamientoModelo.js
@@ -86,8 +86,8 @@ function EntrenamientoModelo() {
                 </div>
                 <div className="columna-derecha">
                     <h1>Entrenamiento del Modelo</h1>
-                    <p>Se optó por el algoritmo de ensemble conocido como RandomForest debido a sus múltiples ventajas: combina varios árboles de decisión para proporcionar predicciones más precisas, es robusto, tiene una excelente capacidad para manejar el overfitting y permite determinar la importancia de las características. Para su implementación, se utilizó el modelo RandomForestClassifier de la biblioteca sklearn.</p>
-                    <p>Con el objetivo de maximizar la eficacia del modelo, se llevó a cabo una búsqueda en malla para optimizar hiperparámetros como el número de árboles, profundidad máxima y mínimas muestras para dividir, entre otros. El rendimiento del modelo se midió utilizando el F1 score. Una vez determinados los hiperparámetros ideales, se evaluó el rendimiento del modelo utilizando un conjunto de datos no etiquetados.</p>
+                    <p>Se optó por el algoritmo de regularización conocido como Ridge Classifier debido a sus múltiples ventajas: es eficiente para tratar con multicolinealidad en los datos, minimiza el impacto de características menos importantes y penaliza grandes coeficientes para prevenir el sobreajuste. Para su implementación, se utilizó el modelo RidgeClassifier de la biblioteca sklearn junto con el método TFIDF para transformar los datos textuales en características numéricas.</p>
+                    <p>Con el objetivo de maximizar la eficacia del modelo, se llevó a cabo una búsqueda en malla para optimizar hiperparámetros como el parámetro de regularización alpha. El rendimiento del modelo se midió utilizando el F1 score. Una vez determinados los hiperparámetros ideales, se evaluó el rendimiento del modelo utilizando un conjunto de datos no etiquetados.</p>
                     <input 
                         type="file" 
                         accept=".xlsx" 
